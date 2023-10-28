d4c003ffdebbfb1767eee3f6f0f6c6d846e5a29c
Il_Mapsi <massante.lorenzo@icloud.com>
2023-10-28 18:41:31 +0200

    modifiche varie per far funzionare i programmi

--- Reader/new/AP_DataRead_Monitor_press-test.py
+++ Reader/new/AP_DataRead_Monitor_press-test.py
@@ -48,8 +48,8 @@ GOOD = True
 #inputPath = "../AstroPlano_sep21/"	#path input file
 #inputFile = "AP_ven_cfg_old" 		# file corrotto (sipm>11)
 
-inputPath = "./"		        # path input file
-inputFile = "20231012-2104"
+inputPath = "./misure/AP20231027-1631/"		        # path input file
+inputFile = "20231027-1631"
 #inputFile = "20220517-1201"
 #inputFile = "20220428-0940"   	    # input file name without AP and extension
 #inputFile = "20220321-2128"   	    # input file name without extension
@@ -63,7 +63,7 @@ output_file = inputFile+ "provaPlot" + "_dst.txt"
 output_log = inputFile+ "_WSVT.txt"
 
 #numero di eventi da processare (-1 = tutti)
-NEVMAX =  5000000   # 9999999    
+NEVMAX =  -1   # 9999999    
 
 # costanti che potremmo dover cambiare
 time_win = 4000    # ns,  lunghezza max finestra temporale di un evento
