33e3157a8a8b185dbc1563867de047bba8367ed0
Il_Mapsi <massante.lorenzo@icloud.com>
2024-03-09 07:28:01 +0100

    new AstroPlanoMar24 with working relay

---
+++ detector/AstroPlanoMar24/astrosettings.py
@@ -0,0 +1,105 @@
+# variables to be shared across modules
+#
+real_daq =  True   # False  # per eseguirlo in simulazione mettere falso
+real_sensors = True   # False # if you have no sensor set it False
+
+if real_daq:    print('\n Acquisizione reale (non simulata)\n')
+else:           print('\n ACQUISIZIONE SIMULATA !!!\n')
+
+mydebug =   True  #False
+STARTED =   0  #stato dell'acquisizione
+READY   =   1
+RUNNING   = 2
+
+# questa classe serve solo a conservare dati comuni a grafica e acquisizione
+class  astrodata():
+    N_CHAN = 12      # n. canali WB
+    N_SiPM = N_CHAN  # n. SiPM
+    VERSIONE = 'Ottobre 2023'
+    # intervallo di tempo in secondi fra 2 correzioni di tensione per variazioni di T
+    HV_correct_interval = 600
+    V_alarm = 11.7
+
+    Stato = STARTED
+    MyGui = None
+    guiActive = False  # per evitare di scrivere su finestra inesistente
+    scroll    = None    # indirizzo dell'area di scroll della GUI
+    link_plot = None
+
+# valori default dei parametri
+    configfilepath = "./"
+    configfile     = 'AstroPlano2L2T2.cfg'
+    homedir   = './'    # path del logfile e dell'output file, devono coincidere
+    log_file  = None
+    logf      = None  # file pointer
+    outfile   = None
+    outfiledefault = 'AP_log_default'
+    durata    = 2	    	# durata del run
+    time_unit = 0  		    # 0 minuti,  1 ore
+    monitpath = homedir     # path e filename del file usato per monitoring
+    monitfile = outfiledefault
+    MyPipeName  = "AstroPlanoPipe" # pipe per trasmettere comandi a WB
+    stop_analisi= False  # per  arrestare lettura file monitoring
+
+
+#   parametri WB
+    leading   = 15  # valore ragionevole di default, uguale per tutti i canali
+    trailing  = 25
+    pedestal  = 0X3E00  # piedistalli, a priori tutti uguali
+    gain      = 0  # 0 basso, 1 alto
+    mode      = 0 # 0 normale, 1 angolare
+    SiPM_HV   = [55.93, 55.9, 55.12, 55.0, 55.65, 55.35, 55.62, 55.84, 55.8, \
+             55.61, 55.96, 55.87] # tensioni in Volt
+    iob_delay = '25 20 25 20 28 28 20 20 25 25 22 22'
+#   tensioni corrette per la temperatura
+    SiPM_HV_corr =[-1.0]*N_SiPM
+    
+# soglia iniziale e finale
+    thr_start = [20., 20., 20., 20., 20., 20., 20., 20., 20., 20., 20., 20.]
+    thr_stop  = [15., 15., 15., 15., 15., 15., 15., 15., 15., 15., 15., 15.]
+	
+# sensori  pressione, temperatura, umidita' relativa
+    Temperature = None
+    Pressure    = None
+    RelHumid    = None 
+    T_sensors   = [None]*4   #  i 4 sensori di temperatura
+    V_values    = [None]*4   # lettura tensioni
+	
+# Dati per correzioni temperatura    
+    Treference = 20.0
+    Tempco     = 54.0   # 54 mV/ C
+    TempcoFlag = False  # falso -> no correzione
+    
+#parametri per connessione con WB, non toccare
+    ip         = None   # indirizzo IP della WB,  qui 169.254.180.39
+    port       = 22
+    remoteuser = "root"
+    keypath    = "/home/pi/.ssh/id_rsa"
+
+# pin relè
+    relay_ch   = 26
+    
+    
+# funzioni di uso comune
+
+def cleanFilename(nome, ext):
+    # se il nome contiene una estensione (.xyz) non faccio nulla, altrimenti 
+    # aggiungo ext
+    
+    if '.' in nome[-4:-1]:  # c'e' un punto tra gli ultimi 4 caratteri?
+        return nome         # c'e' una estensione, non faccio nulla
+    elif '.' == nome[-1]:   # e' l'ultimo carattere
+        return nome + ext
+    else:
+        return nome + '.' + ext   # aggiungo punto piu' estensione
+    
+if __name__ == '__main__':   
+    nomeprova = cleanFilename('nomedelfile', 'bin')
+    print ('%s \n'% nomeprova)
+    
+    nomeprova = cleanFilename('nomedelfile.', 'bin')
+    print ('%s \n'% nomeprova)
+    
+    nomeprova = cleanFilename('nomedelfile.t', 'bin')
+    print ('%s \n'% nomeprova)
+   
